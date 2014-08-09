227c825d0fcf5d65cb4759281ab2d796cf50236c
cdrvfe <cdrvfe5029@gmail.com>
2014-08-10 01:07:41 +0900

    検証用クラスTestCoreを追加、startの動作修正

--- start.rb
+++ start.rb
@@ -1,9 +1,10 @@
 require './gamesystem'
-require './core'
+require './testcore'
+
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
-core = Core.new
-game = GameSystem.new(Core.new, SCREEN_WIDTH, SCREEN_HEIGHT)
+core = TestCore.new
+game = GameSystem.new(core, SCREEN_WIDTH, SCREEN_HEIGHT)
 game.start
