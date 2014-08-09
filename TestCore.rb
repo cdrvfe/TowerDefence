4fa98cccb0bde13bf22d5b54c2d0f8c943d15ae5
cdrvfe <cdrvfe5029@gmail.com>
2014-08-10 01:57:24 +0900

    フィールド描写結合・確認

--- TestCore.rb
+++ TestCore.rb
@@ -1,9 +1,16 @@
 require './Core.rb'
+require './Field.rb'
 
 class TestCore < Core
+
+	def initialize
+		@field = Field.new
+	end
+
 	def update
 	end
 
 	def draw(screen)
+		@field.draw(screen)
 	end
 end
