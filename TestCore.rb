63082fafd37b92a09bdba8bf94d80d12bbe38750
skylove.exe@gmail.com <skylove.exe@gmail.com>
2014-08-10 04:34:47 +0900

    TestCoreCanCallEnemy

--- TestCore.rb
+++ TestCore.rb
@@ -1,10 +1,12 @@
 require './Core.rb'
 require './Field.rb'
+require './Enemy.rb'
 
 class TestCore < Core
 
 	def initialize
 		@field = Field.new
+		@enemy = Enemy.new(Route.new)
 	end
 
 	def update
@@ -12,5 +14,6 @@ class TestCore < Core
 
 	def draw(screen)
 		@field.draw(screen)
+		@enemy.draw(screen)
 	end
 end
